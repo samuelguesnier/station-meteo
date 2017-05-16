bfb561316d2a009db0a6f20daffebcb85b888889
samuelguesnier <samuel.guesnier@etudiant.univ-rennes1.fr>
2017-05-16 17:01:16 +0200

    indentation

--- V2.js
+++ V2.js
@@ -7,60 +7,30 @@ var bodyParser     =  require("body-parser");
 var app = express();
 
 var temp =" "
-
-var temperature = " " 
-
+var temperature = " "
 var hh = " "
-
-var humidity = " " 
-
+var humidity = " "
 var restApi = rest('http://192.168.1.88/', {
-
-  crossDomain: true
-
+crossDomain: true
 });
 
-
-
-
-
 restApi.read('users/der-On/repos', function(error, data) {
-
-
-
-
-
  temperature = data.variables.temperature ;
-
  hh = data.variables.humidity ;
-
- humidity = hh ; 
-
- temp = temperature;
-
+ humidity = hh ;
+temp = temperature;
 temperature = "";
 
 console.log(temp);
-
 console.log(hh) ;
 
 ;});
 
 app.use(express.static('public'));
-
-
-
 app.get('/', function (req, res) {
 
-
-
-console.log(temp);
-
-  
-
-  res.render('index.ejs',{temperature: temp, humidity:humidity})
-
-	
+    console.log(temp);
+    res.render('index.ejs',{temperature: temp, humidity:humidity})
 
 })
 
